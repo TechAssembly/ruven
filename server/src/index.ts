e207de9b4789a3a90d21260b0067daafde59f3b2
dviryamin <dviryamin@outlook.com>
2018-11-09 14:31:37 +0200

    add root endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import { Server } from 'colyseus';
 import { createServer } from 'http';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import { GameRoom } from './Rooms/GameRoom/GameRoom';
 
 const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Game is up');
+});
 
 const gameServer = new Server({
   server: createServer(app),
