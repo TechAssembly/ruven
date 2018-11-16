b0176b3b1feebafea091f7fd3075d1f1c033ae47
Oren <outbreaker99@gmail.com>
2018-11-16 13:53:41 +0200

    Evil Watcher model with animations, waypoint network script with some waypoints, animator controller

---
+++ client/Assets/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum PathDisplayMode { None, Connections, Paths }
+
+public class AIWaypointNetwork : MonoBehaviour
+{
+    [HideInInspector]
+    public PathDisplayMode DisplayMode = PathDisplayMode.Connections;
+
+    [HideInInspector]
+    public int From;
+
+    [HideInInspector]
+    public int To;
+
+    public List<Transform> Waypoints = new List<Transform>();
+}
